8fac0f5569eb332e5bbef5a0223ace6249bb1df5
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-07-25 18:16:33 +0000

    Solution bootstrap for dotnet elements

---
+++ src/dotnet/Datapoint.Compass/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.IO;
+
+namespace Datapoint.Compass
+{
+    internal static class Program
+    {
+        internal static void Main(string[] args)
+        {
+            var hostConfiguration = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .AddEnvironmentVariables("ASPNETCORE_")
+                .AddEnvironmentVariables("COMPASS_")
+                .Build();
+
+            new WebHostBuilder()
+                .UseConfiguration(hostConfiguration)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseKestrel()
+
+                .Configure((hostContext, app) =>
+                {
+                    app.UseRouting();
+
+                    app.UseEndpoints((endpoints) =>
+                    {
+                        endpoints.MapFallbackToFile("index.html");
+                    });
+                })
+
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddRouting();
+                })
+
+                .Build()
+                .Run();
+        }
+    }
+}
