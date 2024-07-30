707c62131f7290f8eca0379bb46ff09aa60f2835
Joao Lopes <joao.pl.lopes@datapoint.pt>
2024-07-30 10:42:40 +0000

    Workspace enrollment header and breadcrumbs

--- src/angular/src/app/components/workspace/enrollments/update/workspace-enrollment-update.component.ts
+++ src/angular/src/app/components/workspace/enrollments/update/workspace-enrollment-update.component.ts
@@ -1,6 +1,8 @@
 import { Component } from "@angular/core";
+import { RouterLink } from "@angular/router";
 
 @Component({
+  imports: [ RouterLink ],
   selector: "app-workspace-enrollment-update",
   standalone: true,
   templateUrl: "workspace-enrollment-update.component.html"
